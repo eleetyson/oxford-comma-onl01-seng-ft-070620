9057f76f0c89bc048c90c093ad27bc97af8b150d
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-07 17:36:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   arr = []
   array.each_with_index do |element, index|
     if array.size <= 2
-      if 
+      if index
       arr << ", and " + element
     else
       arr << element + ", "
